c6fe0fe02c9be9499e6d5958286bbe612d277b61
kabeer_ Akanni <kabeerlap@gmail.com>
2021-12-30 18:39:20 +0100

    udacity

--- designs.js
+++ designs.js
@@ -1,5 +1,39 @@
+// The tip is that, we are using the Javascript language to "manipulate" an existing webpage built with HTML and CSS. Think of HTML for a second. We are creating "elements" as we code like the paragraphs and other stuff like ;
+
+// (<p>, <table>, <span> etc.)
+// To do this project we need to pick them one by one using JavaScript codes like ;
+
+// //'pick' the elements like so;
+// document.getElementById('inputHeight')
+// which you "pick" the element with your code and do some "programming" on it.
+
+// For example;
+
+// document.getElementById('sizePicker').addEventListener(....)
+// Above code "picks" the element 'sizePicker' from the DOM and adds an event listener to it so we can interact with it.,
+
+// Mentors, of course can provide a more solid answer. I just tried to give you some ideas if you felt same like me. I was completely lost after I finished the JS lessons. Mr. Kalehoff has an amazing energy and his passion of sharing his information is obvious from his gestures even but the JS section of this NanoDegree needs some modifications I believe. Good luck with the project and happy coding.
+
+
+
+// You will need to access the inputs and form elements. These may help: document.getElementById or document.querySelector. Don't forget to assign them to variables.
+// Then, you need to listen when a user submits the form with addEventListener.
+// Inside the submit listener, you'll need a function which builds the grid. Get the inputs value and loop through them, adding rows to the table (you will need to get the table element) and cells to each row. insertRow and insertCell are your friends here.
+// You will need to add click listeners to paint cells as well.
+// One of the behaviors you want is: when a user clicks on a cell, paint it. If you have the cell in a variable, what about adding a click event right after creating it? And you want to change its background (remember: CSS "color" property is to change the font color) by the colorPicker value. This may help: https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp
+
+
+
+
+
+
+
+
 // Select color input
 // Select size input
+
+
+
 
 // When size is submitted by the user, call makeGrid()
 
