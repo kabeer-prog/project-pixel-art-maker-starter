576a636b6cfc72dadb9e0eb72322ad9271663e50
kabeer_ Akanni <kabeerlap@gmail.com>
2022-01-11 22:49:05 +0100

    Update JS

--- designs.js
+++ designs.js
@@ -1,88 +1,56 @@
-// The tip is that, we are using the Javascript language to "manipulate" an existing webpage built with HTML and CSS. Think of HTML for a second. We are creating "elements" as we code like the paragraphs and other stuff like ;
-
-// (<p>, <table>, <span> etc.)
-// To do this project we need to pick them one by one using JavaScript codes like ;
-
-// //'pick' the elements like so;
-// document.getElementById('inputHeight')
-// which you "pick" the element with your code and do some "programming" on it.
-
-// For example;
-
-// document.getElementById('sizePicker').addEventListener(....)
-// Above code "picks" the element 'sizePicker' from the DOM and adds an event listener to it so we can interact with it.,
-
-// Mentors, of course can provide a more solid answer. I just tried to give you some ideas if you felt same like me. I was completely lost after I finished the JS lessons. Mr. Kalehoff has an amazing energy and his passion of sharing his information is obvious from his gestures even but the JS section of this NanoDegree needs some modifications I believe. Good luck with the project and happy coding.
-
-
-
-// You will need to access the inputs and form elements. These may help: document.getElementById or document.querySelector. Don't forget to assign them to variables.
-// Then, you need to listen when a user submits the form with addEventListener.
-// Inside the submit listener, you'll need a function which builds the grid. Get the inputs value and loop through them, adding rows to the table (you will need to get the table element) and cells to each row. insertRow and insertCell are your friends here.
-// You will need to add click listeners to paint cells as well.
-// One of the behaviors you want is: when a user clicks on a cell, paint it. If you have the cell in a variable, what about adding a click event right after creating it? And you want to change its background (remember: CSS "color" property is to change the font color) by the colorPicker value. This may help: https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp
-
-
-
-
-
-
-
 
 // Select color input 
 // Select size input(height, width)
-var height, width, color;
 
-// when the submit button is clicked, after the size of the grid is inputted, call function makeGrid()
-// $('#sizePicker').submit(function (event){
-//     event.preventDefault();
-//     height = $('#inputHeight').val();
-//     width =$('#inputWidth').val();
-//     makeGrid(height, width);
-//     // console.log("height is " + height +" and" + "weight is "+ width)
-// })
-document.querySelector("#sizePicker").addEventListener('click', formSubmission)
 
-function formSubmission(){
-    event.preventDefault();
+//"picks" the element with id 'sizePicker' from the DOM and adds an event listener to it so we can interact with it
+document.querySelector("#sizePicker").addEventListener('click', submit)
+
+function submit(e){
+    e.preventDefault();
+    
     let height = document.getElementById('inputHeight').value;
     let width = document.getElementById('inputWidth').value;
+    
+    // pass on the values of height and width from the inputs to makeGrid function
+    
     makeGrid(height, width);
-
+    
 }
-function makeGrid (height, width){
 
+
+
+
+function makeGrid(height, width){
+//     picks" the element with id 'pixelCanvas' i.e table from the DOM,set it to a variable table
     let table = document.getElementById('pixelCanvas');
-    for ( var i = 0; i <= height; i++){
-        // table.append('<tr id=tabl' + i + '></tr>');
-        table.append(<tr></tr>);
-        for (var j= 0; j <= width; j++){
-            $('#tabl' + i).append('<td></td>');
-        }
+    
+    for (let i = 1; i< height; i++){
+        //insert row to the table
+        
+      let tableRow = table.insertRow();
+      for (let j = 1; j < width; j++){
+          //insert cells to the rows
+        let column = tableRow.insertCell();
+        // pick and prepare the cell to be clicked from the table and add an event listener to it so it can pick vaalues of the color 
+        column.addEventListener('click', addColorToCell);
+      }
+     
+    }
 }
-}
+  
+  function addColorToCell(event) {
+    color = document.querySelector("#colorPicker");
+    //Accessing the DOM using methods of the document object
+    event.target.style.backgroundColor = color.value;
 
-// When size is submitted by the user, call makeGrid()
+ 
+  }
 
-// function makeGrid(x, y) {
+  
 
-// // Your code goes here!
-//         $('tr').remove();
-//         for ( var i = 1; i <= x; i++){
-//             $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-//             for (var j= 1; j <= y; j++){
-//                 $('#table' + i).append('<td></td>');
-//             }
-//         }
-//         ///add color to cell after creation while it's been clicked
-//         $('td').click(function addColor(){
-//             color =$('#colorPicker').val();
 
-//             if ($(this).attr('style')){
-//                 $(this).removeAttr('style')
-//             }else{
-//                 $(this).attr("style", "background-color:" + color);
-//             }
-//         })
-// }
+
+
+
 
